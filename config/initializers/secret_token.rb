044f63449372acd7b3822ba86b20fb3f690fba7c
Darius Chatfield <dazchatfield@gmail.com>
2011-07-03 19:26:56 +0100

    initial refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebaymgr::Application.config.secret_token = 'a3cbac89eca089d1bc5ae3bf44ced09980e58939599cb04a304b25021936afd1c1feb80811cafd9ade73973bb30834dbec12e473bbfc2bebee66ea4b80cfbe8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Ebaymgr::Application.config.session_store :cookie_store, :key => '_ebaymgr_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Ebaymgr::Application.config.session_store :active_record_store
